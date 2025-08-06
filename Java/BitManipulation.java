ed4304a77b38deb72bc51665e1664cb6704e8c38
Vintage-Vivek <viveksir58@gmail.com>
2025-08-07 00:19:44 +0530

    Day 6

--- Java/BitManipulation.java
+++ Java/BitManipulation.java
@@ -1,32 +1,48 @@
+import java.util.Scanner;
+
 class BitManipulation {
     public static void main(String [] args){
-        int n = 5; //0101
-        int pos = 2;
-        int bitMask= 1<<pos;
+        // int n = 5; //0101
+        // int pos = 2;
+        // int bitMask= 1<<pos;
 
-        // Get Bit
+        // // Get Bit
 
-        if((bitMask & n) == 0){
-            System.out.println("Bit was 0");
-        } else {
-            System.out.println("Bit was 1");
-        }
+        // if((bitMask & n) == 0){
+        //     System.out.println("Bit was 0");
+        // } else {
+        //     System.out.println("Bit was 1");
+        // }
 
-        // Set Bit
-        int newNumber = bitMask | n;
-        System.out.println(newNumber);
+        // // Set Bit
+        // int newNumber = bitMask | n;
+        // System.out.println(newNumber);
 
-        // Clear bit
-        int notBitMask = ~(bitMask);
+        // // Clear bit
+        // int notBitMask = ~(bitMask);
 
-        int clear = n & notBitMask;
-        System.out.println(clear);
+        // int clear = n & notBitMask;
+        // System.out.println(clear);
 
         // Update Bit
-        int m = 5;
-        int position = 1;
-        int operation = 1; // update bit to 1;
-        
-        
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+        int oper = sc.nextInt(); 
+
+        // oper=1 : set, oper=0 : clear
+        int n = 5;   // 0101
+        int pos = 1;
+
+        int bitMask = 1 << pos;
+        if (oper == 1) {
+            // set
+            int newNumber = bitMask | n;
+            System.out.println(newNumber);
+        } else {
+            // clear
+            int newBitMask = ~(bitMask);
+            int newNumber = newBitMask & n;
+            System.out.println(newNumber);
+        }
     }
 }
