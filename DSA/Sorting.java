08246f1a7a6a59705e5f7d9bd1852cf8b8d49d81
Vintage-Vivek <viveksir58@gmail.com>
2025-08-07 22:13:20 +0530

    Day 7

--- DSA/Sorting.java
+++ DSA/Sorting.java
@@ -51,23 +51,21 @@ class Sorting {
     }
 
     static void insertionSort(){
-        int [] arr = {7,8,3,1,2};
+            int arr[] = { 7, 8, 3, 1, 2 };
 
-        for (int i = 0; i < arr.length-1; i++) {
-            int sorted = i;
-            for (int j = i+1; j < arr.length-1; j++) {
-                int curr = i;
-                int next = j;
-                if(arr[curr] < arr[next]){
-                    int temp = arr[next];
-                    arr[sorted] = arr[curr];
-                    arr[next] = temp; 
-                } else if (arr[curr] > arr[next]){
-                    sorted++;
-                }
-                
+        // insertion sort
+        for (int i = 1; i < arr.length; i++) {
+            int current = arr[i];
+            int j = i - 1;
+            while (j >= 0 && current < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
             }
+
+            // placement
+            arr[j + 1] = current;
         }
+
         printArray(arr);
     }
 
