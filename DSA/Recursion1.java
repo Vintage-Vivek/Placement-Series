43fb0aebacc819d671cf23265cbc7c61474d7d47
Vintage-Vivek <viveksir58@gmail.com>
2025-08-09 23:28:30 +0530

    Day 9

--- DSA/Recursion1.java
+++ DSA/Recursion1.java
@@ -11,9 +11,21 @@ class Recursion1{
         System.out.println(n);
         printNumb2(n+1);
     }
+    static void printSum(int i, int n, int sum){
+        if( i == n){
+            sum += i; 
+            System.out.println(sum);
+            return;
+        }
+        sum += i;
+        printSum(i+1, n, sum);
+        System.out.println(i);
+
+    }
     public static void main(String[] args) {
-        int n = 1;
+        // int n = 5;
         // printNumb(n);
-        printNumb2(n);
+        // printNumb2(n);
+        printSum(1,5,0);
     }
 }
