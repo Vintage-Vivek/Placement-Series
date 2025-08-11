0f7250c934935907fcf7afb11d33cf8614ce7481
Vintage-Vivek <viveksir58@gmail.com>
2025-08-11 22:51:23 +0530

    Day 11

--- DSA/Recursion2.java
+++ DSA/Recursion2.java
@@ -12,8 +12,41 @@ public class Recursion2 {
         towerOfHanoi(n-1, help, src, des);
     }
 
+    static void reverseString(int idx, String str){
+        if(idx<=0){
+            System.out.print(str.charAt(idx));
+            return;
+        }
+        System.out.print(str.charAt(idx));
+        reverseString(idx-1, str);
+    }
+
+    static int first = -1;
+    static int last = -1;
+
+    static void findOccurance (String str, int idx, char element){
+        if(idx == str.length()){
+            System.out.println(first);
+            System.out.println(last);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if(currChar == element){
+            if(first == -1){
+                first = idx;
+            } else {
+                last = idx;
+            }
+        }
+        findOccurance(str, idx+1, element);
+    }
+
     public static void main(String[] args) {
-        int n = 3;
-        towerOfHanoi(n, "S", "H", "D");
+        // int n = 3;
+        // towerOfHanoi(n, "S", "H", "D");
+        // String str = "abcd";
+        // int idx = str.length()-1;
+        // reverseString(idx, str);
+        findOccurance("baacdaefaaha", 0, 'a');
     }
 }
