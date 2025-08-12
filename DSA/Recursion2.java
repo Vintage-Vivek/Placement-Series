a5b0a2275828534e1cf051a6df2c809860771530
Vintage-Vivek <viveksir58@gmail.com>
2025-08-12 23:55:47 +0530

    Day 12

--- DSA/Recursion2.java
+++ DSA/Recursion2.java
@@ -1,5 +1,7 @@
 package DSA; 
 
+import java.util.HashSet;
+
 public class Recursion2 {
     
     static void towerOfHanoi(int n,String src, String help, String des){
@@ -40,13 +42,137 @@ public class Recursion2 {
         }
         findOccurance(str, idx+1, element);
     }
+    
+    static boolean isSorted(int [] arr, int idx){
+        if(idx == arr.length-1){
+            return true;
+        }
+        // To the point logic
+        // if(arr[idx] < arr[idx+1]){
+        //     return isSorted(arr,idx+1);
+        // } else {
+        //     return false;
+        // }
+        // Reversed logic
+        if(arr[idx] >= arr[idx+1]){
+            return false;
+        } else {
+            return isSorted(arr, idx+1);
+        }
+    }
+    
+    static void moveAllX(String str, int idx, int count, String newString){
+        if(idx == str.length()){
+            for(int i = 0; i < count; i++){
+                newString += 'x';
+            }
+            System.out.println(newString);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if(currChar == 'x'){
+            count++;
+            moveAllX(str, idx+1, count, newString);
+        } else {
+            newString += currChar;
+            moveAllX(str, idx+1, count, newString);
+        }
+    }
+
+    static boolean [] map = new boolean[26];
+
+    static void removeDuplicates(String str, int idx, String newString){
+        if(idx == str.length()){
+            System.out.println(newString);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if(map[currChar - 'a']){
+            removeDuplicates(str, idx+1, newString);
+        } else {
+            newString += currChar;
+            map[currChar - 'a'] = true;
+            removeDuplicates(str, idx+1, newString);
+        }
+    }
+
+    static void printSubsequence(String str, int idx, String newString){
+        if(idx == str.length()){
+            System.out.println(newString);
+            return;
+        }
+
+        char currChar = str.charAt(idx);
+        // To be in new string
+        printSubsequence(str, idx+1, newString+currChar);
+
+        // Not to be 
+        printSubsequence(str, idx+1, newString);
+
+    }
+
+    static void uniqueSubsequence(String str, int idx, String newString, HashSet<String> set){
+
+        if(idx == str.length()){
+            if(set.contains(newString)){
+                return;
+            } else {
+                set.add(newString);
+                System.out.println(newString);
+                return;
+            }
+        }
+
+        char currChar = str.charAt(idx);
+        // To be
+        uniqueSubsequence(str, idx+1, newString+currChar, set);
+        // Not to be
+        uniqueSubsequence(str, idx+1, newString, set);
+
+    }
+
+    static String [] keypad = {".","abc","def","ghi","jkl","mno","pqrs","tuv","wx","yz"};
+
+    static void printCombo(String str, int idx, String combination){
+
+        if(idx == str.length()){
+            System.out.println(combination);
+            return;
+        }
+
+        char currChar = str.charAt(idx);
+        String mapping = keypad[currChar - '0'];
+        for(int i = 0; i < mapping.length(); i++){
+            printCombo(str, idx+1, combination+mapping.charAt(i));
+        }
+    }
 
     public static void main(String[] args) {
         // int n = 3;
         // towerOfHanoi(n, "S", "H", "D");
+
         // String str = "abcd";
         // int idx = str.length()-1;
         // reverseString(idx, str);
-        findOccurance("baacdaefaaha", 0, 'a');
+
+        // findOccurance("baacdaefaaha", 0, 'a');
+
+        // int [] arr = {1,2,3,4,5};
+        // System.out.println(isSorted(arr, 0));
+
+        // String str = "axbcxxd";
+        // moveAllX(str, 0, 0, "");
+
+        // String str = "abbccda";
+        // removeDuplicates(str, 0, "");
+
+        // String str = "abc";
+        // printSubsequence(str, 0, "");
+
+        // String str = "aaa";
+        // HashSet <String> set = new HashSet<>();
+        // uniqueSubsequence(str, 0, "", set);
+        
+        printCombo("23",0,"");
     }
 }
