c79c2e37c1cb6f584ae27311f2929bd0574c6bf2
Vintage-Vivek <viveksir58@gmail.com>
2025-08-15 23:47:51 +0530

    Day 15

--- DSA/Recursion3.java
+++ DSA/Recursion3.java
@@ -1,5 +1,7 @@
 package DSA;
 
+import java.util.*;
+
 public class Recursion3 {
 
     static void printPermutation(String str, String permutation){
@@ -13,6 +15,7 @@ public class Recursion3 {
             char currChar = str.charAt(i);
             // abc -> bc
             String newString = str.substring(0,i)+str.substring(i+1);
+            // str.substring(0,i) -> "" and str.substring(i+1) -> "bc" then furthur iterations
             printPermutation(newString, permutation+currChar);
         }
     }
@@ -31,13 +34,79 @@ public class Recursion3 {
         return downPaths+rightPaths;
     }
 
+    static int waysToPlaceTiles(int n, int m){
+
+        if (n == m) return 2;
+
+        if(n < m) return 1; 
+
+        // If we put first Horizontally (__)
+        int horizontalPlacement = waysToPlaceTiles(n-1, m);
+        
+        // If we put first vertical (|)
+        int verticalPlacement = waysToPlaceTiles(n-m, m);
+
+        return horizontalPlacement+verticalPlacement;
+        
+    }
+
+    static int callGuests(int n){
+
+        if(n<=1) return 1;
+
+        // single
+        int way1 = callGuests(n-1);
+
+        // pair 
+        int way2 = (n-1)*callGuests(n-2);
+
+        return way1+way2;
+    }
+
+    static void printSubsets(ArrayList <Integer> subsets){
+        System.out.print("( ");
+        for (int i = 0; i < subsets.size(); i++) {
+            System.out.print(subsets.get(i)+" ");
+        }
+        System.out.print(" )");
+        System.out.println();
+    }
+
+    static void findSubsets(int n, ArrayList <Integer> subsets) {
+
+        if(n==0) {
+            printSubsets(subsets);
+            return;
+        }
+
+        // "if the number will be added"
+        subsets.add(n);
+        findSubsets(n-1, subsets);
+
+        // "if the number will not be added"
+        subsets.remove(subsets.size()-1);
+        findSubsets(n-1, subsets); 
+
+    }
+
     public static void main(String[] args) {
 
         // String str = "abc";
         // printPermutation(str, "");
 
-        int n = 3, m = 3;
-        int totalPaths = countPaths(0, 0, n, m);
-        System.out.println(totalPaths);
+        // int n = 3, m = 3;
+        // int totalPaths = countPaths(0, 0, n, m);
+        // System.out.println(totalPaths);
+
+        // int n = 6, m = 4;
+        // System.out.println(waysToPlaceTiles(n, m));
+
+        // int n = 4;
+        // System.out.println(callGuests(n));
+
+        int n = 3;
+        ArrayList <Integer> subsets = new ArrayList<>(); 
+        findSubsets(n, subsets);
+        
     }
 }
---
+++ Java/Revision2.java
@@ -0,0 +1,111 @@
+import java.util.Scanner;
+
+public class Revision2 {
+    static void switchCase(int n){
+        switch(n){
+            case 1: 
+            System.out.println("Red");
+            break;
+            case 2: 
+            System.out.println("Yellow");
+            break;
+            case 3: 
+            System.out.println("Green");
+            break;
+            default: 
+            System.out.println("Go");
+        }
+    }
+
+    static void printSolidRect(int n){
+        for (int i = 1; i <= n ; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void invertedHalfPyramid180Degree(int n){
+        
+        for (int i = 1; i <= n; i++) {
+
+            for (int k = n-i; k > 0; k--) {
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void butterfly(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            for (int j = 1; j <= 2*(n-i); j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = n-1; i >= 1 ; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            for (int j = 1; j <= 2*(n-i); j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void diamondPattern(int n){
+        for (int i = 1; i < n; i++) {
+            for (int j = n-i; j >= 1; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= (2*i)-1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = n-1; i >= 1; i--) {
+            for (int j = n-i; j >= 1; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= (2*i)-1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void main(String[] args) {
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner (System.in);
+
+        // int n = sc.nextInt();
+        // switchCase(n);
+
+        // printSolidRect(4);
+
+        // invertedHalfPyramid180Degree(4);
+
+        // butterfly(4);
+
+        diamondPattern(4);
+
+        
+
+
+    }
+}
