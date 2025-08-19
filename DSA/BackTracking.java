9725f0c9038d8a168201fb78318730a6c64a9b4d
Vintage-Vivek <viveksir58@gmail.com>
2025-08-19 23:18:46 +0530

    Day 19

---
+++ DSA/BackTracking.java
@@ -0,0 +1,27 @@
+package DSA;
+
+public class BackTracking {
+
+    static void printPermutation(String str, String permutations){
+
+        if(str.length()==0){
+            System.out.println(permutations);
+            return;
+        }
+
+        for (int j = 0; j < str.length(); j++) {
+            
+            char curr = str.charAt(j);
+            String newStr = str.substring(0,j) + str.substring(j+1);
+
+            printPermutation(newStr, permutations+curr);
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        
+        printPermutation("abc", "");
+    }
+}
